475a1f2a1f4c6de031aa0cdb4dfd0670216ac433
Tejaswini <tchilakanti@gmail.com>
2026-02-01 19:12:01 +0530

    11111 pattern

---
+++ pgm2.java
@@ -0,0 +1,10 @@
+public class pgm2 {
+    public static void main(String args[]){
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                System.out.print(i+1);
+            }
+            System.out.println();
+        }
+    }
+}
