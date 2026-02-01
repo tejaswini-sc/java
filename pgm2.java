b8a6ed5fa4a7e8392e5660d89ea514c5c203b683
Tejaswini <tchilakanti@gmail.com>
2026-02-01 19:44:51 +0530

    holo pattern

--- pgm2.java
+++ pgm2.java
@@ -22,6 +22,29 @@ public class pgm2 {
             System.out.println();
         }
     }
+    static void num(){
+        int count=1;
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                if(count<10){
+                    System.out.print("0");
+                }
+                System.out.print(count++ + " ");
+            }
+            System.out.println();
+        }
+    }
+    static void mulnum(){
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                if(i*j<10){
+                    System.out.print("0");
+                }
+                System.out.print(i*j + " ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String args[]){
         for(int i=0;i<5;i++){
             for(int j=0;j<5;j++){
@@ -33,5 +56,9 @@ public class pgm2 {
         pattern();
         System.out.println();
         holo();
+        System.out.println();
+        num();
+        System.out.println();
+        mulnum();
     }
 }
