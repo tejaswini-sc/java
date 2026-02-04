f8aa046d6695aa64a2788797f747b85256b80d15
Tejaswini <tchilakanti@gmail.com>
2026-02-04 22:45:16 +0530

    assignment pattern2

--- Assignment.java
+++ Assignment.java
@@ -15,7 +15,21 @@ public class Assignment {
             System.out.println();
         }
     }
-    
+       static void pt3(){
+        int n=5;
+        for(int i=0;i<n;i++){
+            // int count=i+1;
+            for(int j=0;j<n;j++){
+                if(j<=i){
+                    System.out.print(j+1);
+                }
+                else{
+                    System.out.print("1");
+                }
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         int n=5;
         for(int i=0;i<n;i++){
