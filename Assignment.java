f7903469f1d97ad4306828685ac311e23305ace8
Tejaswini <tchilakanti@gmail.com>
2026-02-04 22:44:35 +0530

    assignment pattern1

--- Assignment.java
+++ Assignment.java
@@ -1,35 +1,21 @@
 public class Assignment {
 
-    // static void pt2(){
-    //     int n=5;
-    //     for(int i=0;i<n;i++){
-    //         // int count=i+1;
-    //         for(int j=0;j<n;j++){
-    //             if(j<=i){
-    //                 System.out.print(i+1);
-    //             }
-    //             else{
-    //                 System.out.print("1");
-    //             }
-    //         }
-    //         System.out.println();
-    //     }
-    // }
-    //    static void pt3(){
-    //     int n=5;
-    //     for(int i=0;i<n;i++){
-    //         // int count=i+1;
-    //         for(int j=0;j<n;j++){
-    //             if(j<=i){
-    //                 System.out.print(j+1);
-    //             }
-    //             else{
-    //                 System.out.print("1");
-    //             }
-    //         }
-    //         System.out.println();
-    //     }
-    // }
+    static void pt2(){
+        int n=5;
+        for(int i=0;i<n;i++){
+            // int count=i+1;
+            for(int j=0;j<n;j++){
+                if(j<=i){
+                    System.out.print(i+1);
+                }
+                else{
+                    System.out.print("1");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
     public static void main(String[] args) {
         int n=5;
         for(int i=0;i<n;i++){
@@ -43,8 +29,8 @@ public class Assignment {
             }
             System.out.println();
         }
-        // System.out.println();
-        // pt2();
+        System.out.println();
+        pt2();
         // System.out.println();
         // pt3();
     }
