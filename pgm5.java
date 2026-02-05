bef97b9d3f6eaa5f369c3dcabb0f4447cfc96a48
Tejaswini <tchilakanti@gmail.com>
2026-02-05 20:18:57 +0530

    part2 pattern7

--- pgm5.java
+++ pgm5.java
@@ -288,6 +288,26 @@ public class pgm5 {
             System.out.println();
         }
     }
+    static void pt7()
+    {
+        int n=5;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+        for(int i=1;i<=(n-1);i++)
+        {
+            for(int j=1;j<=(n-i);j++)
+            {
+                System.out.print(n-i);
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String args[]){
         for(int i=0;i<5;i++){
@@ -340,6 +360,8 @@ public class pgm5 {
         pt5();
          System.out.println();
         pt6();
+         System.out.println();
+        pt7();
 
     }
 }
