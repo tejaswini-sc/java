e951007a4e26cd62257767df362dde58c3b937aa
Tejaswini <tchilakanti@gmail.com>
2026-02-05 19:08:37 +0530

    part2 pattern1

--- pgm5.java
+++ pgm5.java
@@ -192,6 +192,18 @@ public class pgm5 {
             System.out.println();
         } 
     }
+    static void pt1(){
+        int n=5;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=(2*i)-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    
+
     public static void main(String args[]){
         for(int i=0;i<5;i++){
             for(int j=0;j<5;j++){
@@ -231,5 +243,12 @@ public class pgm5 {
         upsidetri();
         System.out.println();
         holeupsidetri();
+        System.out.println();
+        pt1();
+        System.out.println();
+        // pt2();
+        // System.out.println();
+        // pt3();
+
     }
 }
