887a9bc1a70cb8e6c8979c7966c81bfaf337171e
Tejaswini <tchilakanti@gmail.com>
2026-02-06 22:05:22 +0530

    part3 pattern2

--- pgm6.java
+++ pgm6.java
@@ -13,8 +13,26 @@ public class pgm6 {
         }
     
     }
-     
+    static void pt2()
+    {
+        int n=5;
+        for(int i=1;i<=n;i++)
+        {
+           int count=i*(i+1)/2;
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(count-j+1);
+            if(j<=i-1)
+            {
+                System.out.print("*");
+            }
+            }
+        System.out.println();
+        }
+    }
     public static void main(String[] args) {
     pt1();
+    System.out.println();
+    pt2();
    } 
 }
