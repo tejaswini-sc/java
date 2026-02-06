37b7647c9beb5ead9fb4118d164dc3b2b848e2e2
Tejaswini <tchilakanti@gmail.com>
2026-02-06 22:05:55 +0530

    part3 pattern2...

--- pgm6.java
+++ pgm6.java
@@ -21,7 +21,7 @@ public class pgm6 {
            int count=i*(i+1)/2;
             for(int j=1;j<=i;j++)
             {
-                System.out.print(count-j+1);
+                System.out.print(count--);
             if(j<=i-1)
             {
                 System.out.print("*");
