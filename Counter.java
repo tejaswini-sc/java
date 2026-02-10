91a7111ae6459d79b8a02c649f011260b55f1d5d
Tejaswini <tchilakanti@gmail.com>
2026-02-10 20:51:01 +0530

    factorial

--- Counter.java
+++ Counter.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
 
 class Counter{
+    static int fact(int n)
+    {
+        int res = 1;
+        for(int i=1;i<=n;i++)
+        {
+            res=res*i;
+        }
+        return res;
+    }
     static int countDig(int n)
     {
         int count=0;
@@ -15,7 +24,10 @@ class Counter{
         Scanner sc= new Scanner(System.in);
         int n = sc.nextInt();
         int res=countDig(n);
+        int res1 = fact(n);
         System.out.println(res);
+        System.out.println(res1);
+
 
     }
 
