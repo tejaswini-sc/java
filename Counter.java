5cc28571b74477b98a803d2dd11e110eecc363a8
Tejaswini <tchilakanti@gmail.com>
2026-02-10 21:33:03 +0530

    avg of num

--- Counter.java
+++ Counter.java
@@ -20,13 +20,24 @@ class Counter{
         return count;
     }
 
+    static int avg(int a, int b, int c)
+    {
+     int add =a+b+c;
+     int avg=add/3;
+     return avg;   
+    }
+
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         int n = sc.nextInt();
+        int a=sc.nextInt();
+        int b= sc.nextInt();
+        int c = sc.nextInt();
         int res=countDig(n);
         int res1 = fact(n);
         System.out.println(res);
         System.out.println(res1);
+        System.out.println(avg(a, b, c));
 
 
     }
