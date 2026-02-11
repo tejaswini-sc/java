6cc0a0ab2daff1b6404e6c8a8c6134f7846f4ed5
swapna <tchilakanti@gmail.com>
2026-02-11 22:43:50 +0530

    adddd

--- Counter.java
+++ Counter.java
@@ -25,7 +25,19 @@ class Counter{
      int add =a+b+c;
      int avg=add/3;
      return avg;   
-
+    }
+    static int sumofodd(int n)
+    {
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            if(i%2!=0)
+            {
+                sum=sum+i;
+            }
+        }
+        return sum;
+    }
 
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
