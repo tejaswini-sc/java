9e8c4d221c976ff9660881262fae93d12827ef27
swapna <tchilakanti@gmail.com>
2026-02-12 22:33:06 +0530

    operators mcq

---
+++ Operator.java
@@ -0,0 +1,17 @@
+public class Operator {
+    public static void main(String[] args) {
+        byte b= 127;
+        b++;
+        System.out.println(b);
+        int c = 5;
+        int a= 10;
+        System.out.println(c & a);
+        System.out.println("10" + 20 + 30);
+        System.out.println(a++ + ++a);
+        byte d = 10;
+        d = (byte)(d*2); 
+        System.out.println(d);
+        System.out.println(10 / 3);
+
+    }
+}
