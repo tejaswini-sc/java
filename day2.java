627457d825d5e55dbf505efeb88a21defd3a69a0
swapna <tchilakanti@gmail.com>
2026-02-13 23:57:38 +0530

    two digit

--- day2.java
+++ day2.java
@@ -1,3 +1,4 @@
+import java.nio.channels.Pipe.SourceChannel;
 import java.util.Scanner;
 
 public class day2 {
@@ -17,11 +18,23 @@ public class day2 {
         System.out.println(n);
 
     }
+    static void twodigits(int n)
+    {
+        if(n>=10 && n<100)
+        {
+            System.out.println("Number is 2 digit");
+        }
+        else 
+        {
+            System.out.println("number is not 2 digit");
+        }
+    }
     public static void main(String[] args) {
       Scanner scan =new Scanner(System.in);
       int n=scan.nextInt();
       evenodd(n); 
       System.out.println();
       removelastdigit(n); 
+      twodigits(n);
     } 
 }
