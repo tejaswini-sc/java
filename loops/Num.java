8b4606f5ec61d53c85447e005f3d60bdf8705de7
swapna <tchilakanti@gmail.com>
2026-02-14 23:10:06 +0530

    n even nos

--- loops/Num.java
+++ loops/Num.java
@@ -1,10 +1,21 @@
 package loops;
-
 public class Num {
+
+    static void evennum(int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
     public static void main(String[] args) {
         for(int i=1;i<=10;i++)
         {
             System.out.println(i);
         }
+        evennum(25);
     }
 }
