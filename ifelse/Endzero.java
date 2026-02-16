492ce314f449f716b1eefc133bb8dda131deb63a
swapna <tchilakanti@gmail.com>
2026-02-16 15:20:17 +0530

    end with zero

---
+++ ifelse/Endzero.java
@@ -0,0 +1,18 @@
+package ifelse;
+
+import java.util.Scanner;
+
+public class Endzero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%10==0)
+        {
+            System.out.println("yes");
+        }
+        else
+        {
+            System.out.println("no");
+        }
+    }   
+}
