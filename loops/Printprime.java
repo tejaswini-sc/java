9183b030691ffecc34976ca20439bb792df5fcef
swapna <tchilakanti@gmail.com>
2026-02-20 18:49:10 +0530

    Check prime or not TYPE 3

--- loops/Printprime.java
+++ loops/Printprime.java
@@ -31,11 +31,23 @@ public class Printprime {
             return "not prime";
     }
     
+    static String primeOrNot3(int n)
+    {
+        for(int i=2;i<=n/2;i++) // or for(int i=2;i*i<=n;i++)
+        {
+            if(n%i==0)
+                return "not prime";
+        }
+        return "prime";
+    }
+
+    
+
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
        
-        String res = primeOrNot2(n);
+        String res = primeOrNot3(n);
         System.out.println(res);
        }
 }
