7177371b1830473d3c39d8d782fd5732b5ddd10e
swapna <tchilakanti@gmail.com>
2026-02-20 18:48:25 +0530

    Check prime or not TYPE 2

--- loops/Printprime.java
+++ loops/Printprime.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Printprime {
     
-    static String primes(int n)
+    static String primeOrNot1(int n)
     {
         int count =0;
         for(int i=1;i<=n;i++)
@@ -16,10 +16,26 @@ public class Printprime {
         else
             return "not prime";
     }
+
+    static String primeOrNot2(int n)
+    {
+        int count =0;
+        for(int i=2;i<=n/2;i++)
+        {
+            if(n%i==0)
+                count++;
+        }
+        if(count==0)
+            return "prime";
+        else
+            return "not prime";
+    }
+    
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        String res = primes(n);
+       
+        String res = primeOrNot2(n);
         System.out.println(res);
        }
 }
