c9d72fdd433108bfb0a5570d0f7fc39c3d2cec0a
swapna <tchilakanti@gmail.com>
2026-02-21 23:39:22 +0530

    Least common factor

---
+++ loops/Lcf.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Lcf {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        // implement your logic here
+        // int min=Min(n,m);
+        for(int i = 1;i<=n;i--)
+        {
+            if(n%i==0 && m%i==0)
+            {
+                System.out.print(i);
+                break;
+            }
+        }
+    }
+}
