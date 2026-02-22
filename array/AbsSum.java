5fda540dfe1de0cde016c321c2dde3fdf210cfd1
swapna <tchilakanti@gmail.com>
2026-02-22 11:18:52 +0530

    Absoulte array sum

---
+++ array/AbsSum.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class AbsSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // implement your logic here
+        int []a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]<0)
+            {
+                a[i]=-a[i];
+                sum=sum+a[i];
+            }
+            else
+            {
+                sum=sum+a[i];
+            }
+            System.out.println(sum);
+        }
+    }
+}
