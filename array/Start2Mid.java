0f057bfedb238971faec80ed01d09e5c4972d425
swapna <tchilakanti@gmail.com>
2026-02-22 18:39:14 +0530

    print middle length array

---
+++ array/Start2Mid.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Start2Mid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int []a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        System.out.println("values");
+        for(int i=0;i<n/2;i++)
+        {
+            System.out.println(a[i]);
+        }
+    }
+    
+}
