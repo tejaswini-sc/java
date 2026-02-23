f12429405ac1baf1417b25e2aff669e0d82fb5be
swapna <tchilakanti@gmail.com>
2026-02-23 22:47:27 +0530

    print Armstrong numbers

--- array/Armstorng.java
+++ array/Armstorng.java
@@ -24,18 +24,29 @@ public class Armstorng {
         }
         return arm==temp;
     }
-    
+    static void printArmstrong(int n,int m)
+    {
+        for(int i=n;i<=m;i++)
+        {
+            if(armStrong(i))
+            {
+                System.out.println(i);
+            }   
+        }
+    }
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         int n = sc.nextInt();
+        int m = sc.nextInt();
         System.out.println(countDigit(n));
-        if(armStrong(n))
-        {
-            System.out.println("Armstrong");
-        }
-        else{
-            System.out.println("not armstrong");
-        }
+        // if(armStrong(n))
+        // {
+        //     System.out.println("Armstrong");
+        // }
+        // else{
+        //     System.out.println("not armstrong");
+        // }
+        printArmstrong(n, m);
 
     }
     
