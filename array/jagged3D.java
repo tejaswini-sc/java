df5d1dc4bf338a0c8d5baebf1017010e439d69ce
swapna <tchilakanti@gmail.com>
2026-02-23 23:39:37 +0530

    jagged array

--- array/jagged3D.java
+++ array/jagged3D.java
@@ -22,25 +22,7 @@ public class jagged3D {
             }
 
         }
-        // a[0]=new int[2][];
-        // a[1]=new int[3][];
-        // a[0][0]=new int[5];
-        // a[0][1]=new int[4];
-        // a[1][0]=new int[3];
-        // a[1][1]=new int[2];
-        // a[1][2]=new int[6];
 
-        // for(int i=0;i<2;i++)
-        // {
-        //     for(int j=0;j<a[i].length;j++)
-        //     {
-        //         for(int k=0;k<a[i][j].length;k++)
-        //         {
-        //             System.out.print("school "+i+" class "+j+" student "+k);
-        //             a[i][j][k]=sc.nextInt();
-        //         }
-        //     }
-        // }
         for(int i=0;i<2;i++)
         {
             for(int j=0;j<a[i].length;j++)
