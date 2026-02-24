cceab02633c13da3a5294717c052f524225a31b1
swapna <tchilakanti@gmail.com>
2026-02-24 23:21:19 +0530

    Missing Number

---
+++ array/MissingNum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MissingNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();   
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+        for (int i = 0; i < n; i++) {
+            actualSum += nums[i];
+        }
+        int missing = expectedSum - actualSum;
+        System.out.println(missing);
+    }
+}
