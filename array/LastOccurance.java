d17e27aa6605fe197b1341bbc88bd306f42b7bf3
swapna <tchilakanti@gmail.com>
2026-02-24 22:42:56 +0530

    largest occurance

---
+++ array/LastOccurance.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LastOccurance {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // implement your logic here
+        int []a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int max=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]>max)
+                max=a[i];
+        }
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]==max)
+                count++;
+        }
+        System.out.println(count);
+
+    }   
+}
