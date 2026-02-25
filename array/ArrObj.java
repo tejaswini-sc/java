456cc14b345bc9765aa3dd60347053cc28cea07f
swapna <tchilakanti@gmail.com>
2026-02-25 21:58:28 +0530

    Storing obj in Array

--- array/ArrObj.java
+++ array/ArrObj.java
@@ -1,3 +1,7 @@
+package Array;
+
+import java.util.Scanner;
+
 class Emp
 {
 	int id;
@@ -9,21 +13,28 @@ class Emp
 public class ArrObj {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+//		int n=sc.nextInt();
 		Emp e1=new Emp();
 		Emp e2=new Emp();
-		e1.id=1;
-		e1.Name="aaa";
-		e1.Salary=1000;
-		Emp ar[]=new Emp[2];
-		ar[0]=e1;
-		System.out.println(ar[0].id);
-		ar[1]=e2;
-		ar[1].id=3;
-		System.out.println(ar);
-		System.out.println(e1);
-		System.out.println(e1.Name);
-		System.out.println(e1.Salary);
-		System.out.println(ar[0].id);
+		Emp a[]=new Emp[2];
+		a[0]=e1;
+		a[1]=e2;
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.println("enter the id of emp "+ (i+1) +" ");
+			a[i].id=sc.nextInt();
+			System.out.println("enter the name of emp "+ (i+1) +" ");
+			a[i].Name=sc.next();
+			System.out.println("enter the salary of emp "+ (i+1) +" ");
+			a[i].Salary=sc.nextFloat();
+			
+		}
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i].id);
+			System.out.println(a[i].Name);
+			System.out.println(a[i].Salary);
+		}
 		
 	}
 
