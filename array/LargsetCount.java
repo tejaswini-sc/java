12c35fa00df75befe53be73ed34f3d271571e947
swapna <tchilakanti@gmail.com>
2026-02-26 22:45:08 +0530

    Count Largest Element Occurrences

--- array/LargsetCount.java
+++ array/LargsetCount.java
@@ -13,7 +13,10 @@ public class LargsetCount {
         int max=Integer.MIN_VALUE;
         for(int i=0;i<n;i++)
         {
-            
+            if(a[i]>max)
+            {
+                max=a[i];
+            }
         }
         int count=0;
         for(int i=0;i<n;i++)
