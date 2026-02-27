5803c1bcc330fb3d82b481a17cdfe8835c2cbce1
swapna <tchilakanti@gmail.com>
2026-02-27 13:36:36 +0530

    PAir sum with K

--- array/PrintSumPair.java
+++ array/PrintSumPair.java
@@ -3,19 +3,22 @@ public class PrintSumPair {
     public static void main(String args[])
     {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("enter n value");
         int n=scanner.nextInt();
         int a[]=new int[n];
+        System.out.println("enter array values");
         for(int i=0;i<n;i++)
         {
             a[i]=scanner.nextInt();
         }
+        System.out.println("enter k value");
         int k=scanner.nextInt();
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n-1;i++)
         {
-            for(int j=i+1;i<n;i++)
+            for(int j=i+1;j<n;j++)
             {
                 if(a[i]+a[j]==k)
-                    System.out.print(a[i]+" "+a[j]);
+                    System.out.println(a[i]+" "+a[j]);
             }
         }
     }    
