81e483c541f0b5398f441151be185b5519e366f2
swapna <tchilakanti@gmail.com>
2026-02-27 13:40:09 +0530

    Largest repeating elements

---
+++ array/LargestRepeat.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class LargestRepeat {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=scanner.nextInt();
+        }
+        int repeat=-1;
+        boolean found=false;
+        for(int i=a.length-1;i>=1;i--)
+        {
+            if(a[i]==a[i-1])
+            {
+                repeat=a[i];
+                found=true;
+                System.out.print(repeat);
+
+                break;
+            }
+            
+        }
+        if(!found)
+            System.out.print(repeat);
+
+    }
+}
