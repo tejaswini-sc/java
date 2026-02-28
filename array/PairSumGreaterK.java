2fcd877932ad1d46e32af449c32fae6059a95b30
swapna <tchilakanti@gmail.com>
2026-02-28 21:19:17 +0530

    Count Pairs

--- array/PairSumGreaterK.java
+++ array/PairSumGreaterK.java
@@ -4,12 +4,15 @@ public class PairSumGreaterK {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("enter size");
         int n = scanner.nextInt();
         int a[]=new int[n];
+        sSystem.out.println("enter array values");
         for(int i=0;i<n;i++)
         {
             a[i]=scanner.nextInt();
         }
+        sSystem.out.println("enter k value");
         int k=scanner.nextInt();
         for(int i=0;i<n;i++)
         {
