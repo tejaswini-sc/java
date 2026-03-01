da5dd08aef4b3823645f041104fe6a09f501a848
swapna <tchilakanti@gmail.com>
2026-03-01 21:13:30 +0530

    Pairs with Second Value Smaller than First Value

---
+++ array/PrimePairs.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class PrimePairs {
+
+    public static boolean checkPrime(int n)
+    {
+        if(n <= 1)
+            return false;
+        for(int i=2;i*i<=n;i++)
+        {
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=scanner.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(checkPrime(a[i]) || checkPrime(a[j]))
+                {
+                    System.out.println(a[i]+" "+a[j]);
+                }
+            }
+        }
+        
+    }
+}
