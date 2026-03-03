9e3dd133a248fab5699674b1edc78e34f320b78f
tejaswini-sc <tchilakanti@gmail.com>
2026-03-03 15:54:43 +0530

    print elements in array

---
+++ array/SortedArray/PrintUniqueNum.java
@@ -0,0 +1,27 @@
+package SortedArray;
+import java.util.*;
+public class PrintUniqueNum {
+    public static void nonrepate(int a[])
+    {
+        for(int i=0;i<a.length-1;i++)
+        {
+            if(a[i]!=a[i+1])
+            {
+                System.out.println(a[i]);
+            }
+        }
+        System.out.print(a[a.length-1]);
+    }
+
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        nonrepate(a);
+    }    
+}
