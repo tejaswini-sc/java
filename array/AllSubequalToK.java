2767bad9b83e2a12a9cdb57c2ae5900a82802080
Tejashwini Chilakanti <tchilakanti@gmail.com>
2026-03-08 22:59:51 +0530

    Add AllSubequalToK.java to find subarrays

---
+++ array/AllSubequalToK.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static void subarrayOfSize(int[]arr, int size) {
+        for(int i=0;i<arr.length-size;i++)
+        {
+            for(int j=i;j<i+size;j++)
+            {
+                System.out.print(arr[j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+		int N = scanner.nextInt();
+		int[] arr = new int[N];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scanner.nextInt();
+		}
+		subarrayOfSize(arr,size);
+    }
+}
