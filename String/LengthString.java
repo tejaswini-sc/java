a028e2011be752da76537daa0908e2566120d5c1
tejaswini-sc <tchilakanti@gmail.com>
2026-03-09 22:30:40 +0530

    counth length of tsring

---
+++ String/LengthString.java
@@ -0,0 +1,11 @@
+package String;
+import java.util.*;
+public class LengthString {
+
+    public static void main(String[] args) {
+      Scanner sc =new Scanner(System.in);
+      String word= sc.next();
+      System.out.print(word.length());
+    }    
+}
+
--- Typecast.java
+++ Typecast.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Typecast {
     public static void main(String[] args) {
       Scanner sc =new Scanner(System.in);
-      String word= sc.next();
+      String word= sc.nextLine();
       System.out.print(word);
     }    
 }
