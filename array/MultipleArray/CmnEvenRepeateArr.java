91ef2a21c9e2a44869c7c7a4538da1c4781d48c3
tejaswini-sc <tchilakanti@gmail.com>
2026-03-10 21:31:04 +0530

    Common Repeating Even Elements in Sorted Arrays

---
+++ array/MultipleArray/CmnEvenRepeateArr.java
@@ -0,0 +1,56 @@
+package MultipleArray;
+import java.util.*;
+
+public class CmnEvenRepeateArr {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=scanner.nextInt();
+        }
+        int n1=scanner.nextInt();
+        int b[]=new int[n1];
+        for(int i=0;i<n1;i++)
+        {
+            b[i]=scanner.nextInt();
+        }
+        // for(int i=0;i<n;i++)
+        // {
+        //     for(int j=0;j<n1;j++)
+        //     {
+        //         if( a[i]==b[j])
+        //         {
+        //             if(a[i]%2==0)
+        //             {
+        //             System.out.print(a[i]+" ");
+        //             break;
+        //             }                    
+        //         }
+                
+        //     }
+        // }
+        int i=0,j=0;
+        while(i<n && j<n1)
+        {
+            if(a[i]==b[j])
+            {
+                if(a[i]%2==0)
+                {
+                    System.out.print(a[i]+" ");
+                }
+                i++;
+                j++;
+            }
+            else if(a[i]>b[j])
+            {
+                i++;
+            }
+            else{
+                j++;
+            }
+        }
+    }
+}
