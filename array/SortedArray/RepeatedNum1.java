445c94575771f18d007d18f6410a9e290012f053
tejaswini-sc <tchilakanti@gmail.com>
2026-03-11 16:22:08 +0530

    count subarray with sum k

--- array/SortedArray/RepeatedNum1.java
+++ array/SortedArray/RepeatedNum1.java
@@ -7,6 +7,7 @@ public class RepeatedNum1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+        int min=Integer.MAX_VALUE;
         int a[]=new int[n];
         for(int i=0;i<n;i++)
         {
