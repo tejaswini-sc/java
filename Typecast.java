4e68f0d9b60029ebdbb3abe3af7c4d9daa3ba557
tejaswini-sc <tchilakanti@gmail.com>
2026-03-12 22:03:34 +0530

    Reaarange array -1

--- Typecast.java
+++ Typecast.java
@@ -3,7 +3,14 @@ import java.util.Scanner;
 public class Typecast {
     public static void main(String[] args) {
       Scanner sc =new Scanner(System.in);
-      String word= sc.nextLine();
-      System.out.print(word);
+      // String word= sc.nextLine();
+      // System.out.print(word);
+      int n=sc.nextInt();
+      int a[] =new int[n];
+      System.out.println(a.length/2);
+            System.out.println((a.length+1)/2);
+
+
+
     }    
 }
