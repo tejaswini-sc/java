c60743c83add8edd77b0bfc88e4cc476f3c40f7c
tejaswini-sc <tchilakanti@gmail.com>
2026-03-15 22:00:25 +0530

    Product Inventory Management

---
+++ ProductApp.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Product {
+    private int pId;
+    private String pName;
+    private int quantity;
+    private float price;
+
+    public void setData(int pId, String pName, int quantity, float price) {
+        this.pId = pId;
+        this.pName = pName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public void getId() {
+        System.out.println(pId);
+    }
+
+    public void getName() {
+        System.out.println(pName);
+    }
+
+    public void getQun() {
+        System.out.println(quantity);
+    }
+
+    public void getPrice() {
+        System.out.println(price);
+    }
+}
+
+class ProductApp {   
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Product p = new Product();
+
+        int pId = sc.nextInt();
+        sc.nextLine();
+        String pname = sc.nextLine();
+        int quantity = sc.nextInt();
+        float price = sc.nextFloat();
+
+        p.setData(pId, pname, quantity, price);
+
+        p.getId();
+        p.getName();
+        p.getQun();
+        p.getPrice();
+    }
+}
