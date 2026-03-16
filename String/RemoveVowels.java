fc4db1f55fd14bb8ac05b3f34c6b629efb2eb8c3
tejaswini-sc <tchilakanti@gmail.com>
2026-03-16 21:42:11 +0530

    remove vowels

---
+++ String/RemoveVowels.java
@@ -0,0 +1,24 @@
+package String;
+import java.util.Scanner;
+public class RemoveVowels {
+        public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String t="";
+        for(int i=0;i<input.length();i++)
+        {
+            char ch=input.toLowerCase().charAt(i);
+   
+            if(ch!='a'&& ch!='e'&& ch!='i' && ch!='o' && ch!='u' && ch!='A'&& ch!='E'&& ch!='I' && ch!='O' && ch!='U')            
+            {
+                t=t+ch;
+            }
+            
+        }
+        System.out.println(t);
+      
+    }    
+}
+
+
+
