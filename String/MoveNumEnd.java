004f47a9187041221b17fe7cd0c4070400414df2
tejaswini-sc <tchilakanti@gmail.com>
2026-03-16 21:58:57 +0530

    Move Numerics at end

---
+++ String/MoveNumEnd.java
@@ -0,0 +1,23 @@
+package String;
+import java.util.Scanner;
+
+public class MoveNumEnd {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String s="";
+        String t="";
+        for(int i=0;i<input.length();i++)
+        {
+            char ch=input.charAt(i);
+            if(ch>='A' && ch<='Z' || ch>='a' && ch<='z'){
+                t=t+ch;
+            }
+            else{
+                s=s+ch;
+            }
+        }
+       System.out.print(t+s);
+    }
+}
