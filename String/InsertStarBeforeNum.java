0b17d259f2e5470e0568f810d6c791f6c865141f
tejaswini-sc <tchilakanti@gmail.com>
2026-03-18 20:57:12 +0530

    insert star before 0 to 9

---
+++ String/InsertStarBeforeNum.java
@@ -0,0 +1,25 @@
+package String;
+import java.util.Scanner;
+
+public class InsertStarBeforeNum {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        String t="";
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(ch>='0' && ch<='9')
+            {
+                t=t+'*'+ch;
+            }
+            else{
+                t=t+ch;
+            }
+        }
+        System.out.println(t);
+    
+    }
+
+}
