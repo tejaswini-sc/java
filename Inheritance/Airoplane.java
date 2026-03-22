b1a00b63ae7e41f1f049c718ee90088c30ae657a
tejaswini-sc <tchilakanti@gmail.com>
2026-03-22 13:29:28 +0530

    inheritance Airoplane

---
+++ Inheritance/Airoplane.java
@@ -0,0 +1,52 @@
+package Inheritance;
+class Plane
+{
+	void takeoff() {
+		System.out.println("plane is take of");
+	}
+	
+	void fly() {
+		System.out.println("plane is Fly");
+	}
+	
+	void land() {
+		System.out.println("plane is Land");
+	}
+}
+
+class PassengerPlane extends Plane
+{
+	@Override
+	void fly() {
+		System.out.println("Passenger flies at medium");
+
+	}
+	void carryPassenger() {
+		System.out.println("carry People");
+	}
+
+}
+
+class FlightPlane extends Plane
+{
+	 @Override
+	void fly() {
+			System.out.println("Passenger flies at Height");
+	}
+	 void carryWepoma() {
+			System.out.println("carry wepona");
+	 }
+}
+
+
+public class Airoplane {
+
+	public static void main(String[] args) {
+		CragoPlane cp =new CragoPlane();
+		cp.takeoff();
+		cp.fly();
+		cp.land();
+		cp.carrycargo();
+	}
+
+}
