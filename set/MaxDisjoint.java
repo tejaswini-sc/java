17b8fb2cec4f684bb3927a77c0ea9bce2e6a00de
tejaswini-sc <tchilakanti@gmail.com>
2026-03-30 16:34:25 +0530

    MaxDisjoint

---
+++ set/MaxDisjoint.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class MaxDisjoint {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (set.contains(arr[i])) {
+                // duplicate found → form a group
+                count++;
+                set.clear(); // start new group
+            } else {
+                set.add(arr[i]);
+            }
+        }
+
+        System.out.println(count);
+    }
+}
