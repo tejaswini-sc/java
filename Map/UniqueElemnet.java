14c6e712ece1d9b2da78d100feae0f0ffaf56827
tejaswini-sc <tchilakanti@gmail.com>
2026-03-31 15:50:11 +0530

    Unique Elements Finder

---
+++ Map/UniqueElemnet.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class UniqueElemnet {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        // Input
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Frequency count
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(a[i])) {
+                int count = map.get(a[i]);
+                map.put(a[i], count + 1);
+            } else {
+                map.put(a[i], 1);
+            }
+        }
+
+        for (int key : map.keySet()) {
+            int value = map.get(key);
+
+            if (value == 1) {
+                System.out.println(key);
+            }
+        }
+
+    }
+}
