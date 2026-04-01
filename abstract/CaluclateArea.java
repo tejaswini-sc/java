c7a60e36850f0f454fb1b189b55103ca0fe24eca
tejaswini-sc <tchilakanti@gmail.com>
2026-04-01 22:26:11 +0530

    Calculate area using abstract

---
+++ abstract/CaluclateArea.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+
+// Abstract class Shape
+abstract class Shape {
+    static Scanner sc = new Scanner(System.in); // ✅ single shared Scanner
+    float area;
+
+    abstract void acceptInput();
+    abstract void calcArea();
+
+    float getArea() {
+        return area;
+    }
+}
+
+// Subclass Square
+class Square extends Shape {
+    float side;
+
+    @Override
+    void acceptInput() {
+        side = sc.nextFloat();
+    }
+
+    @Override
+    void calcArea() {
+        area = side * side;
+    }
+}
+
+// Subclass Rectangle
+class Rectangle extends Shape {
+    float length, breadth;
+
+    @Override
+    void acceptInput() {
+        length = sc.nextFloat();
+        breadth = sc.nextFloat();
+    }
+
+    @Override
+    void calcArea() {
+        area = length * breadth;
+    }
+}
+
+// Subclass Circle
+class Circle extends Shape {
+    float radius;
+
+    @Override
+    void acceptInput() {
+        radius = sc.nextFloat();
+    }
+
+    @Override
+    void calcArea() {
+        area = (float)(Math.PI * radius * radius);
+    }
+}
+
+// Main class
+public class CaluclateArea {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String type = scanner.next(); // ✅ important: use next()
+
+        Shape s;
+
+        if(type.equals("Square")){
+            s = new Square();
+        }
+        else if(type.equals("Rectangle")){
+            s = new Rectangle();
+        }
+        else if(type.equals("Circle")){
+            s = new Circle();
+        }
+        else{
+            System.out.println("Invalid Shape");
+            return;
+        }
+
+        s.acceptInput();
+        s.calcArea();
+        System.out.println("Area of " + type + ": " + s.getArea());
+    }
+}
