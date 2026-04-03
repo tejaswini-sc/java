927412ea8160a781d46808896dd3cf8585cd419a
tejaswini-sc <tchilakanti@gmail.com>
2026-04-03 22:03:49 +0530

    Exception

--- Exception/StringException.java
+++ Exception/StringException.java
@@ -1,17 +1,22 @@
 package Exception;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class StringException {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        String s= sc.nextLine();
-        int index=sc.nextInt();
+       
         try{
+             String s= sc.nextLine();
+            int index=sc.nextInt();      
             System.out.print(s.charAt(index));
         }
         catch(StringIndexOutOfBoundsException e){
             System.out.println("invalid index for string");
+        }
+        catch(InputMismatchException e){
+            System.out.println("enter valid input");
         }
     }
 }
