54792640af10fe247551408729e0631fde1d9161
tejaswini-sc <tchilakanti@gmail.com>
2026-04-05 22:08:52 +0530

    custom Exception....

--- Exception/CustomException.java
+++ Exception/CustomException.java
@@ -5,7 +5,12 @@ class UnderAgeException extends Throwable{
     public String getMessage() {
         return "You are too Young";
     }
-
+}
+class OverAgeException extends Throwable{
+    @Override
+    public String getMessage() {
+        return "Your are too old";
+    }
 }
 class CustomException{
     public static void main(String[] args) {
@@ -16,8 +21,17 @@ class CustomException{
             if(age<18){
                 throw new UnderAgeException();
             }
+            else if(age>60){
+                throw new OverAgeException();
+            }
+            else{
+                System.out.println("Eligiblity confirm");
+            }
         }
         catch(UnderAgeException e){
+            System.out.println(e.getMessage());
+        }
+        catch(OverAgeException e){
             System.out.println(e.getMessage());
         }
     }
