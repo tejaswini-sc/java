0ea55ab32d07b7f9fb521600560b1736a27d5a44
tejaswini-sc <tchilakanti@gmail.com>
2026-04-15 22:58:40 +0530

    string reverse first and last

--- String/reverseFirstlast.java
+++ String/reverseFirstlast.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 class reverseFirstlast{
-    // static void reverse(String s){
-    //     char first=s.charAt(0);
-    //     char last=s.charAt(s.length()-1);
-    //     String t= "";
-    //     String mid=s.substring(1,s.length()-1);
-    //     System.out.print(last+mid+first+" ");
-    // }
+    static void reverse(String s){
+        char first=s.charAt(0);
+        char last=s.charAt(s.length()-1);
+        String t= "";
+        String mid=s.substring(1,s.length()-1);
+        System.out.print(last+mid+first+" ");
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String s= sc.nextLine();
