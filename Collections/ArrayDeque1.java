a5c99c9f8dcf643565cf285e6c132aacfccbc112
tejaswini-sc <tchilakanti@gmail.com>
2026-04-16 22:35:19 +0530

    java

---
+++ Collections/ArrayDeque1.java
@@ -0,0 +1,14 @@
+package Collections;
+import java.util.*;
+
+public class ArrayDeque1 {
+
+	public static void main(String[] args) {
+        ArrayDeque<String> dq = new ArrayDeque<>();
+        dq.add("teju");
+        dq.add("swapna");
+        dq.add("aaa");
+        System.out.println(dq);
+        
+	}
+}
