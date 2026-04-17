bcd1011c83f9df43e3e6c9de224324149a132060
tejaswini-sc <tchilakanti@gmail.com>
2026-04-17 15:27:25 +0530

    Arraydeque problems

--- Collections/ArrayDeque1.java
+++ Collections/ArrayDeque1.java
@@ -5,10 +5,40 @@ public class ArrayDeque1 {
 
 	public static void main(String[] args) {
         ArrayDeque<String> dq = new ArrayDeque<>();
-        dq.add("teju");
-        dq.add("swapna");
-        dq.add("aaa");
+        // dq.add("1AC");
+        // dq.add("2AC");
+        // dq.add("3AC");
+
+        // dq.addFirst("VIP");
+        // System.out.println(dq);
+
+        // System.out.println(dq.removeLast());
+        //         System.out.println(dq);
+
+        // dq.add("Google");
+        // dq.addLast("Youtube");
+        // dq.addLast("Linkdin");
+        // System.out.println(dq);
+        // System.out.println(dq.removeLast());
+        // System.out.println(dq);
+
+        // dq.add("BTM");
+        // dq.add("Silk board");
+        // dq.add("Electronic city");
+        // dq.addFirst("emergency stop");
+        // System.out.println(dq);
+        // dq.removeLast();
+        // System.out.println(dq);
+        // System.out.println(dq.peekFirst());
+        // System.out.println(dq.peekLast());
+
+
+        dq.add("A");
+        dq.add("B");
+        dq.add("c");
         System.out.println(dq);
-        
+        dq.removeLast();
+        dq.removeLast();
+        System.out.println(dq);
 	}
 }
