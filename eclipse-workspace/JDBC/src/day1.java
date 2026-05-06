cbc597ce5dbf3327b3de8de2f95a79c326b57124
tejaswini-sc <tchilakanti@gmail.com>
2026-05-06 13:07:13 +0530

    jdbc select query

--- eclipse-workspace/JDBC/src/day1.java
+++ eclipse-workspace/JDBC/src/day1.java
@@ -1,15 +1,43 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class day1 {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String url="jdbc:mysql://localhost:3306/student";
+		String username="root";
+		String password="teju";
+		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			System.out.println("driver loaded");
+			
+			Connection conn = DriverManager.getConnection(url, username, password);
+			System.out.println("connection established");
+			
+			Statement stm = conn.createStatement();
+			
+			String query="select *from course where course_title = 'java'";
+			
+			ResultSet res = stm.executeQuery(query);
+			System.out.println("excute query");
+			
+			while(res.next()) {
+				int ccode =res.getInt("course_code");
+				String title=res.getString("course_title");
+				int credit =res.getInt("credit_value");
+				
+				System.out.println(ccode+" "+" "+title+" "+credit+" ");				
+			}
+			
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
 	}
-
 }
