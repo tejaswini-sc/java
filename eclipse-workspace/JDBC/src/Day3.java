3df0a4cf31b39930be04e5ab382a5b58ccf17603
tejaswini-sc <tchilakanti@gmail.com>
2026-05-08 20:06:07 +0530

    jdbc CRUD operations

---
+++ eclipse-workspace/JDBC/src/Day3.java
@@ -0,0 +1,40 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.*;
+
+public class Day3 {
+	static final String url ="jdbc:mysql://localhost:3306/student";
+	static final String username="root";
+	static final String password="teju";
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		
+		try {			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			Connection con = DriverManager.getConnection(url, username, password);
+			
+			String query="update course set credit_value=? where course_code=?";
+			
+			int cc=sc.nextInt();
+			int cv=sc.nextInt();
+			
+			cc.setInt(1,cc);
+			cv.setInt(2,cv);
+			
+			PreparedStatement ptsmt =con.prepareStatement(query);
+			
+			int executeUpdate = ptsmt.executeUpdate();
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
